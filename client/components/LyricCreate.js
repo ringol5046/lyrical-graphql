2e63d1249832f30576abefd65634b40d01bb6eba
ringol5046 <ringol5046@gmail.com>
2018-02-03 17:08:04 -0600

    lyricCreate component

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class LyricCreate extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: ''};
+  }
+  onSubmit(event) {
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Add a Lyric</label>
+        <input 
+          value={this.state.content}
+          onChange={event => {this.setState({ content: event.target.value })}}
+        />
+      </form>
+    );
+  }
+}
+
+export default LyricCreate;
---
+++ client/components/songDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
+
+class SongDetail extends React.Component {
+  render() {
+    const { song } = this.props.data;
+
+    if(!song) { return <div>Loading...</div>; }
+    return (
+      <div>
+        <Link to='/'>Back</Link>
+        <h3>{song.title}</h3>
+        <LyricCreate />
+      </div>
+    );
+  }
+}
+
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id }}}
+})(SongDetail);
